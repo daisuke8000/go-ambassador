b4c231245629265c9e4f05e5d56e4038826d0ec0
daisuke8000 <daisuke12108000@gmail.com>
2021-10-11 08:16:05 +0900

    links key add

---
+++ src/commands/populateProducts.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"ambassador-backend/src/database"
+	"ambassador-backend/src/models"
+	"github.com/bxcodec/faker/v3"
+	"math/rand"
+)
+
+func main() {
+	database.Connect()
+	for i := 0; i < 30; i++{
+		product := models.Product{
+			Title: faker.Username(),
+			Description: faker.Username(),
+			Image: faker.URL(),
+			Price: float64(rand.Intn(90) + 10),
+		}
+
+		database.DB.Create(&product)
+	}
+}
--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -28,13 +28,41 @@ func CreateProducts(c *fiber.Ctx) error {
 }
 
 func GetProduct(c *fiber.Ctx) error {
-	var product models.Product
-
 	id, _ := strconv.Atoi(c.Params("id"))
+
+	var product models.Product
 
 	product.Id = uint(id)
 
 	database.DB.Find(&product)
 
 	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{}
+	product.Id = uint(id)
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Model(&product).Updates(&product)
+
+	return c.JSON(product)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{}
+	product.Id = uint(id)
+
+	database.DB.Model(&product).Delete(&product)
+
+	return c.JSON(fiber.Map{
+		"message": "deleted successfully. to Id: " + c.Params("id"),
+	})
 }
