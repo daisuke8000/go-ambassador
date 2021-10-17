312cb7c2466a0d49ddd3d841b6fd5d6053401b52
daisuke8000 <daisuke12108000@gmail.com>
2021-10-18 06:08:40 +0900

    checkout endpoint

--- src/database/redis.go
+++ src/database/redis.go
@@ -1,8 +1,14 @@
 package database
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
 
 var Cache *redis.Client
+var CacheChannel chan string
 
 func SetupRedis(){
 	Cache = redis.NewClient(&redis.Options{
@@ -10,4 +16,26 @@ func SetupRedis(){
 		DB: 0,
 
 	})
+}
+
+func SetupCacheChannel() {
+	CacheChannel = make(chan string)
+
+	go func(ch chan string){
+		for {
+			time.Sleep(5 * time.Second)
+
+			key := <- ch
+
+			Cache.Del(context.Background(), key)
+
+			fmt.Println("clear cache " + key)
+		}
+	}(CacheChannel)
+}
+
+func ClearCache(keys ...string){
+	for _, key := range keys {
+		CacheChannel <- key
+	}
 }
